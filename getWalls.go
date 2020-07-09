a1679c9b22bd83dc72d594cdc3bd71f983894f93
Abbaskt <abbask.51tr@gmail.com>
2020-07-09 13:33:18 +0530

    validURL, prepareDirectory and verifySubreddit functions added

--- getWalls.go
+++ getWalls.go
@@ -1,15 +1,72 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	// "os"
+)
 
-func successSum(a,b int)(int){
-	return a+b
+func successSum(a, b int) int {
+	return a + b
 }
 
-func failSum(a,b int)(int){
-	return a-b
+func failSum(a, b int) int {
+	return a - b
 }
 
-func main(){
+func validURL(URL string) bool {
+	resp, err := http.Get(URL)
+	fmt.Print(resp.Status)
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+
+	if resp.StatusCode == 404 {
+		return false
+	}
+
+	return true
+}
+
+func prepareDirectory(directory string) bool {
+	usr, _ := user.Current()
+	directory = usr.HomeDir + directory
+	err := os.MkdirAll(directory, os.ModePerm)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func verifySubreddit(subreddit string) bool{
+	URL := "https://reddit.com/r/" + subreddit
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", URL , nil)
+	if err != nil {
+			log.Fatalln(err)
+	}
+
+	req.Header.Set("User-Agent", "Go_Wallpaper_Downloader")
+
+	resp, err := client.Do(req)
+	if err != nil {
+			log.Fatalln(err)
+	}
+	if resp.StatusCode == 200 {
+		return true
+	}
+	return false
+}
+
+func main() {
 	fmt.Println("Hello World")
+	// valid := validURL("http://i.imgur.com/Z6kdWmA.jpg")
+	// valid := prepareDirectory("/Pictures/Wallpapers/Reddit/")
+	// valid := verifySubreddit("unixporn")
+	// print(valid)
 }
--- getWalls_test.go
+++ getWalls_test.go
@@ -15,3 +15,24 @@ func TestSumWillFail(t *testing.T){
        t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 3)
 	}
 }
+
+func TestValidURL(t *testing.T){
+	resp := validURL("http://i.imgur.com/Z6kdWmA.jpg")
+	if resp == false {
+		t.Errorf("Not a valid URL")
+	}
+}
+
+func TestPrepareDirectory(t *testing.T){
+	resp := prepareDirectory("/Pictures/Wallpapers/Reddit")
+	if resp == false {
+		t.Errorf("Funtion failed to create directory")
+	}
+}
+
+func TestVerifySubreddit(t *testing.T){
+	resp := verifySubreddit("wallpapers")
+	if resp == false {
+		t.Errorf("Subreddit invalid")
+	}
+}
